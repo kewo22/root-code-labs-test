07871fe91679d68e075758081597ae27357f2eb6
KewinFernando <kewinf271@gmail.com>
2022-08-21 14:22:22 +0530

    total added

--- src/pages/Bid/Bid.tsx
+++ src/pages/Bid/Bid.tsx
@@ -1,47 +1,50 @@
 import { FC, ReactElement, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+
+import { CurrencyFormatter } from '../../helpers/currency-formatter';
+
 import { VehicleBid } from "../../interfaces/vehicleBid";
 
 import './style.scss'
 
 const Bid: FC = (): ReactElement => {
 
-    const vehicleBids = useSelector((state: any) => state.vehicles.vehicleBids);
-    const [sum, setSum] = useState(0);
+    const vehicleBids: any[] = useSelector((state: any) => state.vehicles.vehicleBids);
+    const [sum, setSum] = useState('');
 
     useEffect(() => {
         const sum = vehicleBids.reduce((accumulator: number, object: VehicleBid) => {
             return accumulator + object.bidAmount;
         }, 0);
-        setSum(sum)
+        setSum(CurrencyFormatter(sum))
     }, [])
 
 
     return (
-        <>
+        <div className="bid-page-wrapper">
             <h1>Biddings</h1>
-            {
-                vehicleBids.length &&
-                vehicleBids.map((vehicleBid: VehicleBid, key: number) => (
-                    <div key={key} className="bid-wrapper">
-                        <div className="vehicle-details">
-                            <img src={vehicleBid.image} alt="vehicle" />
-                            <p>{vehicleBid.name}</p>&nbsp;
-                            <p>{vehicleBid.brand}</p>&nbsp;
-                            <p>{vehicleBid.manufactureYear}</p>
-                        </div>
-                        <div className="bid-amount">
-                            {vehicleBid.bidAmount} LKR
-                        </div>
-                    </div>
-                ))
-            }
 
-            <div>
-                TOTAL {sum}
+            {!vehicleBids.length && <div className="no-bids">No Bids Yet</div>}
+
+            {vehicleBids && vehicleBids.map((vehicleBid, key) => (
+                <div key={key} className="bid-wrapper">
+                    <div className="vehicle-details">
+                        <img src={vehicleBid.image} alt="vehicle" />
+                        <p>{vehicleBid.name}</p>&nbsp;
+                        <p>{vehicleBid.brand}</p>&nbsp;
+                        <p>{vehicleBid.manufactureYear}</p>
+                    </div>
+                    <div className="bid-amount">
+                        {CurrencyFormatter(vehicleBid.bidAmount)} LKR
+                    </div>
+                </div>
+            ))}
+
+            <div className="total">
+                TOTAL {sum} LKR
             </div>
 
-        </>
+        </div>
     );
 
 };
